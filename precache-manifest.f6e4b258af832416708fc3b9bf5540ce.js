394d6712c7acd71eb038b3e77ee0ec292aa5af73
eugen.liakhovich <19081997J@gmail.com>
2019-08-12 16:29:28 +0300

    Updates

---
+++ precache-manifest.f6e4b258af832416708fc3b9bf5540ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23452f2cfd6a70303fbbdbca38e34450",
+    "url": "/pathfinder-folks/index.html"
+  },
+  {
+    "revision": "ccb9b6a49521004f7427",
+    "url": "/pathfinder-folks/static/css/main.01b94365.chunk.css"
+  },
+  {
+    "revision": "fd8784a34132bbbb362d",
+    "url": "/pathfinder-folks/static/js/2.cbdb3ad6.chunk.js"
+  },
+  {
+    "revision": "ccb9b6a49521004f7427",
+    "url": "/pathfinder-folks/static/js/main.b39caf79.chunk.js"
+  },
+  {
+    "revision": "eb9bb2524a1f72f09ebc",
+    "url": "/pathfinder-folks/static/js/runtime~main.4a3c8da6.js"
+  },
+  {
+    "revision": "e8648bbf15e94aafd4db19be9d8c21c5",
+    "url": "/pathfinder-folks/static/media/npc.e8648bbf.csv"
+  }
+]);
